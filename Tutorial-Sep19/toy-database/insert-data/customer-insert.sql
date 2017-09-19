3c30caed85160c58c215bab6e2a91fe943fe46c0
Masud Rahman <masudcseku@gmail.com>
2017-09-19 03:05:12 -0600

    Adding the insert queries

---
+++ Tutorial-Sep19/toy-database/insert-data/customer-insert.sql
@@ -0,0 +1,32 @@
+insert into customer(customerID,firstName,LastName,email,joined)
+values(1,'Masud','Rahman','masud.rahman@usask.ca','2012-09-07');
+
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(2,'Shamima','Yeasmin','s.y@usask.ca','2012-09-07');
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(3,'Anisha','Yeasmin','a.y@usask.ca','2017-05-01');
+
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(4,'Mohammad','Ali','m.a@usask.ca','2012-10-07');
+
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(5,'Ahmed','Instiaque','a.i@usask.ca','2013-10-07');
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(6,'Ram','Saha','r.s@usask.ca','2011-10-07');
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(7,'Max','payne','m.p@usask.ca','2009-10-07');
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(8,'John','Doe','j.d@usask.ca','2001-10-07');
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(9,'Jane','Doe','j.doe@usask.ca','2012-10-17');
+
+insert into customer(customerID,firstName,LastName,email,joined)
+values(10,'Brad','Pitt','b.p@usask.ca','2014-10-07');
---
+++ Tutorial-Sep19/toy-database/insert-data/customerservice-insert.sql
@@ -0,0 +1,46 @@
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(1, 1, 1, '2017-01-01');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(2, 1, 2, '2017-01-02');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(3, 1, 3, '2017-01-03');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(4, 2, 1, '2017-01-01');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(5, 2, 2, '2017-01-02');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(6, 2, 3, '2017-01-03');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(7, 5, 4, '2017-01-01');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(8, 5, 5, '2017-01-02');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(9, 5, 6, '2017-01-03');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(10, 6, 2, '2017-01-01');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(11, 6, 4, '2017-01-02');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(12, 6, 6, '2017-01-03');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(13, 7, 3, '2017-01-01');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(14, 9, 1, '2017-01-02');
+
+insert into customerservice (serviceID,customerID,staffNo,serviceDate)
+values(15, 10, 6, '2017-01-03');
+
+
